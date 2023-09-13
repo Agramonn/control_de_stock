5d3a0722656b55c1a2706cf9a5759731d7abde95
Pedro Gutiérrez <PedroGutierrez08@outlook.com>
2023-09-13 14:15:04 -0700

    Para acceder a las tablas de la base de datos utilizamos DAO

--- control_de_stock/src/main/java/com/alura/jdbc/modelo/Producto.java
+++ control_de_stock/src/main/java/com/alura/jdbc/modelo/Producto.java
@@ -1,52 +1,53 @@
 package com.alura.jdbc.modelo;
 
 public class Producto {
-	
 
-	private Integer id;
-	private String nombre;
-	private String descripcion;
-	private Integer cantidad;
-	
-	public Producto(String nombre, String descripcion, Integer cantidad) {
-		this.nombre = nombre;
-		this.descripcion= descripcion;
-		this.cantidad= cantidad;
-		
-		
-	}
+    private Integer id;
 
-	public Producto(int id, String nombre, String descripcion, int cantidad) {
-		this.id=id;
-		this.nombre=nombre;
-		this.descripcion=descripcion;
-		this.cantidad=cantidad;
-		}
+    private String nombre;
 
-	public String getNombre() {
-		return nombre;
-	}
+    private String descripcion;
 
-	public String getDescripcion() {
-		return descripcion;
-	}
+    private Integer cantidad;
 
-	public Integer getCantidad() {
-		return cantidad;
-	}	
-	
-	public void setId(int id) {
-		this.id=id;
-	}
-	public Integer getId() {
-		return this.id;
-	}
-	@Override
-	public String toString() {
-		return String.format(
-				"{id: %d, nombre: %s, descripcion: %s, cantidad: %s}",
-				this.id, this.nombre, this.descripcion, this.descripcion);
-	}
-	
-	
+    public Producto(String nombre, String descripcion, Integer cantidad) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.cantidad = cantidad;
+    }
+    
+    public Producto(Integer id, String nombre, String descripcion, Integer cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.cantidad = cantidad;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "{ id: %d, nombre: %s, descripcion: %s, cantidad: %d }",
+                this.id, this.nombre, this.descripcion, this.cantidad);
+    }
+
 }
